5cab15e195c00fa9656a8c740036515a4b3d3d7b
florcez <florcezar2016@gmail.com>
2025-09-25 13:58:44 -0300

    Add files via upload

--- utils/clases.py
+++ utils/clases.py
@@ -1,12 +1,57 @@
 #CLASES
 
-class Test:
+class libro:
+
+    #---------------INFO---------------------
+    def __init__(self, name):
+        self.name = name
+
+    def getAutor(self,autor):
+        self.autor = autor
+    
+    def getGenero(self,genero):
+        self.genero = genero
+    
+    def getYear(self, year):
+        self.year_publicacion = year
+    
+    def getDisponible(self, disponible):
+        self.disponible = disponible
+    #---------------------------------------------
+
+    def prestar(self):
+        if self.disponible == True:
+            self.disponible = False
+    
+    def devolver(self):
+        if self.disponible == False:
+            self.disponible = True
+    
+    def esAntiguo(self):
+        if 2025 -self.year_publicacion > 20:
+            return True
+    
+    def mostrarInfo(self):
+        info = [self.name, self.autor, self.genero, self.year_publicacion, self.disponible]
+        return info
+    
+
+class Biblioteca:
     def __init__(self):
-        print("TEST OK")
-
-class Persona:
-    def __init__(self, nombre):
-        self.nombre = nombre
-
-    def saludo(self):
-        print("Hola ", self.nombre)
+        self.listaLibros = {}
+    
+    def agregarLibro(self, libro):
+        self.listaLibros.append(libro)
+    
+    def listarLibros(self):
+        return self.listaLibros
+    
+    def prestarPorTitulo(self, titulo):
+        for i in range(self.listaLibros):
+            if self.listaLibros[i-1][0].lower() == titulo.lower():
+                return True
+    
+    def devolverPorTitulo(self, titulo):
+        for i in range(self.listaLibros):
+            if self.listaLibros[i-1][0].lower() == titulo.lower():
+                return True
